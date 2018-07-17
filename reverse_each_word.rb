bf64a23cfd1e7631b1180a49cfcd9600542d3b40
mguevara717 <marissa.guevara717@gmail.com>
2018-07-17 00:38:46 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(str)
   array = str.split(" ")
 
-  backwards = array.collect do |x|
-    x.reverse!
-
-  end
-  backwards.join(" ")
+  array.collect {|x| x.reverse!}
+  print array.join(" ")
 end
